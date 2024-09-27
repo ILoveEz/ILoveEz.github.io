bfcc07c2231ea23378c9514f15cfe82f696b37d4
ILoveEz <18296541592@163.com>
2024-09-27 10:33:43 +0800

    Site updated: 2024-09-27 10:33:42

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1727404264736";
+const VERSION = "1727404415887";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
